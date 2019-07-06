9872f10d5a9852aea13bbb455f060f5522a4e4bf
rostislav <grigorjew.rostislaw@gmail.com>
2019-07-06 16:57:20 +0300

    Add stack

--- data_structures/stack/Stack.py
+++ data_structures/stack/Stack.py
@@ -1,52 +1,21 @@
 class Stack:
     def __init__(self):
-        self.__items = []
+        self.stack = []
+
+    def size(self):
+        return len(self.stack)
 
     def pop(self):
-        if self.is_empty():
-            raise RuntimeError('Attempt to pop an empty stack')
-        top_idx = len(self.__items) - 1
-        item = self.__items[top_idx]
-        del self.__items[top_idx]
-        return item
+        if self.size():
+            val = self.stack[0]
+            self.stack = self.stack[1:]
+            return val
+        return None
 
-    def push(self, item):
-        self.__items.append(item)
+    def push(self, value):
+        self.stack = [value] + self.stack
 
-    def top(self):
-        if self.is_empty():
-            raise RuntimeError('Attempt to get top of empty stack')
-        top_idx = len(self.__items) - 1
-        return self.__items[top_idx]
-
-    def is_empty(self):
-        return len(self.__items) == 0
-
-
-if __name__ == '__main__':
-    s = Stack()
-    lst = list(range(10 ** 5))
-    lst2 = []
-
-    for k in lst:
-        s.push(k)
-
-    if s.top() == 10 ** 5 - 1:
-        print('Test 1 Passed')
-    while not s.is_empty():
-        lst2.append(s.pop())
-
-    lst2.reverse()
-
-    if lst2 == lst:
-        print('Test 2 Passed')
-
-    try:
-        s.pop()
-    except RuntimeError:
-        print('Test 3 Passed')
-
-    try:
-        s.top()
-    except RuntimeError:
-        print('Test 4 Passed')
+    def peek(self):
+        if self.size():
+            return self.stack[0]
+        return None
