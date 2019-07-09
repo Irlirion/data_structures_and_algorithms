6060d88e55d335e4a3db423e58b111c0b2867a25
rostislav <grigorjew.rostislaw@gmail.com>
2019-07-09 16:17:08 +0300

    Add Bloom filter

---
+++ data_structures/set/BloomFilter.py
@@ -0,0 +1,30 @@
+class BloomFilter:
+
+    def __init__(self, f_len):
+        self.filter_len = f_len
+        self.filter = bytearray(f_len)
+
+    def hash1(self, str1):
+        # 17
+        s = 0
+        for c in str1:
+            code = ord(c)
+            s = (s * 17 + code) % self.filter_len
+        print(s)
+        return s
+
+    def hash2(self, str1):
+        # 223
+        s = 0
+        for c in str1:
+            code = ord(c)
+            s = (s * 223 + code) % self.filter_len
+        print(s)
+        return s
+
+    def add(self, str1):
+        self.filter[self.hash1(str1)] = 1
+        self.filter[self.hash2(str1)] = 1
+
+    def is_value(self, str1):
+        return self.filter[self.hash1(str1)] and self.filter[self.hash2(str1)]
---
+++ tests/test_bloomFilter.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+
+
+class TestBloomFilter(TestCase):
+    def test_hash1(self):
+        self.fail()
+
+    def test_hash2(self):
+        self.fail()
+
+    def test_add(self):
+        self.fail()
+
+    def test_is_value(self):
+        self.fail()
