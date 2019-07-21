adb4fc01aee372ff18ce3c4a7f74e33d5c8c33bc
rostislav <grigorjew.rostislaw@gmail.com>
2019-07-21 12:07:19 +0300

    Add Native cache

---
+++ data_structures/set/NativeCache.py
@@ -0,0 +1,49 @@
+class NativeCache:
+    def __init__(self, sz):
+        self.size = sz
+        self.slots = [None] * self.size
+        self.values = [None] * self.size
+        self.hits = [0] * self.size
+
+    def hash_fun(self, key):
+        return hash(key) % self.size
+
+    def is_key(self, key):
+        i = self.hash_fun(key)
+        if self.slots[i] == key:
+            return True
+        for i in range(self.size):
+            if self.slots[i] == key:
+                return True
+        return False
+
+    def put(self, key, value):
+        i = self.hash_fun(key)
+        if self.slots[i] == key:
+            return
+        if self.slots[i] is None:
+            self.slots[i] = key
+            self.values[i] = value
+        else:
+            for i in range(self.size):
+                if self.slots[i] is None:
+                    self.slots[i] = key
+                    self.values[i] = value
+                    return
+            i = self.hits.index(min(self.hits))
+            self.slots[i] = key
+            self.values[i] = value
+            self.hits[i] = 0
+            return
+
+    def get(self, key):
+        i = self.hash_fun(key)
+        if self.slots[i] == key:
+            self.hits[i] += 1
+            return self.values[i]
+        else:
+            for i in range(self.size):
+                if self.slots[i] == key:
+                    self.hits[i] += 1
+                    return self.values[i]
+        return None
